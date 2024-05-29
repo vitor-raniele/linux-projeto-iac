fe4eb4ef69e4087f6f1ac4292e79100cf03eb72a
vitor-raniele <vitor.raniele@hotmail.com>
2024-05-29 05:00:34 +0000

    Arquivos IaC v.1

---
+++ projetoiac.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Criando os Diretórios"
+
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
+
+echo "Criando os Grupos de Usuário"
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Criando os Usuários"
+
+useradd carlos -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_ADM
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_VEN
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha@123) -G GRP_SEC
+
+echo "Permissões aos diretórios..."
+
+chown root:GRP_ADM /adm/
+chown root:GRP_VEN /ven/
+chown root:GRP_SEC /sec/
+
+chmod 777 /publico
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+
+echo "Fim do script"
+
+
